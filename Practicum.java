6957d6993297b41eb98d87d7741fc92fa577ed05
AlekseyPa <apalyka@gmail.com>
2022-12-01 18:54:30 +0300

    Добавил класс Person

--- Practicum.java
+++ Practicum.java
@@ -1,4 +1,15 @@
 public class Practicum {
-    public static void Practicum(String[] args) {
+    public static void main(String[] args) {
+        Object anyObject = new Person("Михаил");
+        System.out.println(anyObject);
+
+    }
+}
+
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
     }
 }
